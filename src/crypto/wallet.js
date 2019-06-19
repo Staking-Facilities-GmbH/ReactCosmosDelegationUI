97e922d46270203caae1289442434a6539c02450
Flo Liss <flo.liss@tum.de>
2019-06-19 13:08:19 +0200

    Fixed bug with return type

--- src/crypto/wallet.js
+++ src/crypto/wallet.js
@@ -66,7 +66,7 @@ export function createSignedTx(tx, signature) {
 export function createBroadcastBody(signedTx) {
   return JSON.stringify({
     tx: signedTx,
-    mode: `async`
+    mode: `sync`
   })
 }
 
